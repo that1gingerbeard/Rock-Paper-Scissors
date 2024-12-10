d7d7df662a5bf2af5fbc619ad520857d831e327a
Cody McCarthy <184872396+that1gingerbeard@users.noreply.github.com>
2024-12-09 18:55:00 -0700

    Add rps.html and script.js and style.css

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('Hello World!')
-
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors',];
     const randomIndex = Math.floor(Math.random() * choices.length);
